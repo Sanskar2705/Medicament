9aa39434ea25ee80ad902c4b0fdf5e5db583b47b
Sanskar2705 <117569887+Sanskar2705@users.noreply.github.com>
2022-12-30 10:11:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import smtplib
+import  datetime as dt
+import random
+import pytz
+
+country_time_zone = pytz.timezone('Iran')
+country_time = dt.datetime.now(country_time_zone)
+iran_time= country_time.strftime(" %H:%M:%S")
+print(iran_time)
+
+
+
+
+my_email = "samyfroms5@gmail.com"
+password = "oozwguwslxcrytlw"
+# edited
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(my_email, password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="samyfroms5@gmail.com",
+                        msg=f"Subject: Hello\n\nIt's {iran_time} "
+                        )
