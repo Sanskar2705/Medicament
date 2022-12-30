1a803ef15df31c0b5c7a043e0656fce51d536b1b
Sanskar2705 <117569887+Sanskar2705@users.noreply.github.com>
2022-12-30 10:12:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ print(iran_time)
 
 
 
-my_email = "samyfroms5@gmail.com"
-password = "oozwguwslxcrytlw"
+my_email = "***********@gmail.com"
+password = "************"
 # edited
 
 with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.starttls()
     connection.login(my_email, password)
     connection.sendmail(from_addr=my_email,
-                        to_addrs="samyfroms5@gmail.com",
+                        to_addrs="**********@gmail.com",
                         msg=f"Subject: Hello\n\nIt's {iran_time} "
                         )
